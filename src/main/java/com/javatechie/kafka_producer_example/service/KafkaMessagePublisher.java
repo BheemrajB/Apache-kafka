7a42dc19d1a9f772962d6edd44a002fae9a7b44c
BheemRaj <bheemraj8639@gmail.com>
2025-03-12 17:14:53 -0500

    Apache Kafka producer using Springboot example

---
+++ src/main/java/com/javatechie/kafka_producer_example/service/KafkaMessagePublisher.java
@@ -0,0 +1,28 @@
+package com.javatechie.kafka_producer_example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+
+    @Autowired
+    private KafkaTemplate<String,Object> template;
+
+    public void sendMessageToTopic(String message){
+        CompletableFuture<SendResult<String,Object>> future =template.send("INDvsAFG", message);
+        future.whenComplete((result,ex)->{
+            if(ex==null){
+                System.out.println("Sent message = ["+ message + "] with offset value [" + result.getRecordMetadata().offset()+ "]");
+            }
+            else {
+                System.out.println("Unable to send message=[" + message +" ] due to : " +ex.getMessage());
+            }
+        });
+
+    }
+}
