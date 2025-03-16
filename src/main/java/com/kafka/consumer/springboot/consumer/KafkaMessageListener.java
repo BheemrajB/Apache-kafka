6690d1325411917c5fa8e8481ae7f90c1488469e
BheemRaj <bheemraj8639@gmail.com>
2025-03-16 12:54:28 -0500

    minor change

--- src/main/java/com/kafka/consumer/springboot/consumer/KafkaMessageListener.java
+++ src/main/java/com/kafka/consumer/springboot/consumer/KafkaMessageListener.java
@@ -11,28 +11,28 @@ public class KafkaMessageListener
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
     @KafkaListener(topics = "world-cup", groupId= "Group-1")
-    public void consumer(String message)
+    public void consume(String message)
     {
         log.info("consumer consume the message {}", message);
     }
     @KafkaListener(topics="world-cup", groupId = "Group-2")
-    public void consumer1(String message){
+    public void consume1(String message){
         log.info("consumer1 consume the message1 {}", message);
     }
     @KafkaListener(topics="world-cup", groupId= "Group-3")
-    public void consumer2(String message) {
+    public void consume2(String message) {
         log.info("consumer2 consume the message2 {}", message);
     }
     @KafkaListener(topics="world-cup", groupId= "Group-4")
-    public void consumer3(String message) {
+    public void consume3(String message) {
         log.info("consumer3 consume the message3 {}", message);
     }
     @KafkaListener(topics="world-cup", groupId="Group-5")
-            public void consumer4(String message) {
+            public void consume4(String message) {
         log.info("consumer4 consume the message4 {}", message);
     }
     @KafkaListener(topics="world-cup", groupId="Group-6")
-         public void consumer5(String message){
+         public void consume5(String message){
          log.info("consumer consume the message5 {}", message);
         }
 
